4233e3b64267d9c6bd7daa47baef46e2b24892e6
Jaewan Jeon <jaewan.jeon@lotte.net>
2023-11-10 07:37:41 +0000

    first commit

---
+++ eduproject/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["eduproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ eduproject/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"eduproject/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
